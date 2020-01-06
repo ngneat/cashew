6a69b96ea95ed248e5e82dce21fbabfade26e6e7
itay <itayoded1@gmail.com>
2020-01-06 14:43:36 +0200

    support memo by params

--- projects/ngneat/http-cache/src/lib/httpCacheDecorator.ts
+++ projects/ngneat/http-cache/src/lib/httpCacheDecorator.ts
@@ -1,3 +1,7 @@
+import { Observable } from 'rxjs';
+import { shareReplay } from 'rxjs/operators';
+import { ShareReplayConfig } from 'rxjs/src/internal/operators/shareReplay';
+
 /**
  * This decorator will cache the observable's last result,
  * so the next time we call the function it will return the last result instantly.
@@ -11,22 +15,25 @@
  * ```
  *
  */
-import { Observable } from 'rxjs';
-import { shareReplay } from 'rxjs/operators';
-import { ShareReplayConfig } from 'rxjs/src/internal/operators/shareReplay';
-
 export function HttpCache<T = any>(config: ShareReplayConfig = { bufferSize: 1, refCount: false }, ttl?: number) {
-  let call$: Observable<T>;
+  const cache = new Map<string, Observable<T>>();
+
   return function(target: any, propertyKey: string, descriptor: PropertyDescriptor) {
     const method = descriptor.value;
     descriptor.value = function(): Observable<T> {
-      if (call$) {
-        return call$;
+      const key = JSON.stringify(arguments);
+      const cached = cache.get(key);
+
+      if (cached) {
+        return cached;
       }
-      call$ = method.apply(this, arguments).pipe(shareReplay(config));
+
+      const call$ = method.apply(this, arguments).pipe(shareReplay(config));
+      cache.set(key, call$);
+
       if (ttl) {
         setTimeout(() => {
-          call$ = undefined;
+          cache.delete(key);
         }, ttl);
       }
       return call$;
