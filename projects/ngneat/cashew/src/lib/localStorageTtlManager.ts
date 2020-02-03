62259c97b558ece0be2d5df10d989c23efde250b
theblushingcrow <isinai@salesforce.com>
2020-02-03 17:55:47 +0200

    refactor: 💡 Local Storage Ttl

--- projects/ngneat/cashew/src/lib/localStorageTtlManager.ts
+++ projects/ngneat/cashew/src/lib/localStorageTtlManager.ts
@@ -1,29 +1,42 @@
 import { Inject, Injectable } from '@angular/core';
-import { HTTP_CACHE_CONFIG, HttpCacheConfig } from '@ngneat/cashew';
+import { DefaultTTLManager, HTTP_CACHE_CONFIG, HttpCacheConfig } from '@ngneat/cashew';
 import { deleteByRegex } from './deleteByRegex';
 import { getLocalStorage } from './getLocalStorage';
 
 @Injectable()
 export class LocalStorageTtlManager {
+  private readonly ttl: DefaultTTLManager;
   private readonly ttlStorageKey: string;
 
   constructor(@Inject(HTTP_CACHE_CONFIG) private config: HttpCacheConfig) {
     this.ttlStorageKey = `${config.localStorageKey || 'httpCache'}Ttl`;
+    this.ttl = new DefaultTTLManager(config);
   }
 
   isValid(key: string): boolean {
+    const valid = this.ttl.isValid(key);
+    if (valid) {
+      return true;
+    }
     const storage = getLocalStorage(this.ttlStorageKey);
-    return !!storage[key] && storage[key] > new Date().getTime();
+    const validInStorage = !!storage[key] && storage[key] > new Date().getTime();
+    if (validInStorage) {
+      this.ttl.set(key, storage[key]);
+    }
+    return validInStorage;
   }
 
   set(key: string, ttl?: number) {
     const resolveTTL = ttl || this.config.ttl;
     const storage = getLocalStorage(this.ttlStorageKey);
-    storage[key] = resolveTTL;
+    storage[key] = new Date().setMilliseconds(resolveTTL);
     localStorage.setItem(this.ttlStorageKey, JSON.stringify(storage));
+    this.ttl.set(key, ttl);
   }
 
   delete(key?: string | RegExp) {
+    this.ttl.delete(key);
+
     if (!key) {
       localStorage.removeItem(this.ttlStorageKey);
       return;
