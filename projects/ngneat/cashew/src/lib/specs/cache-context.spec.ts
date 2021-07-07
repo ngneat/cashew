f4f2b1ad8a3660a2bdec1aea4bbc934d7fad36f2
Fabien Dehopré <fabien@dehopre.com>
2021-07-07 17:43:16 +0200

    feat: add unit test

---
+++ projects/ngneat/cashew/src/lib/specs/cache-context.spec.ts
@@ -0,0 +1,26 @@
+import { HttpContext, HttpContextToken } from '@angular/common/http';
+import { CACHE_CONTEXT, withCache } from '../cache-context';
+
+describe('withCache', () => {
+  const token = new HttpContextToken<number>(() => 0);
+
+  it('should reuse existing HttpContext when provided', () => {
+    const existingContext = new HttpContext().set(token, 42);
+
+    const result = withCache({ cache: true, ttl: 60000 }, existingContext);
+    expect(result === existingContext).toBeTruthy();
+    const allTokens = Array.from(result.keys());
+    expect(allTokens.length).toEqual(2);
+    expect(result.get(CACHE_CONTEXT)).toEqual({ cache: true, ttl: 60000 });
+    expect(result.get(token)).toEqual(42);
+  });
+
+  it('should create a new HttpContext when no existing context exists', () => {
+    const result = withCache({ cache: true, ttl: 60000 });
+    expect(result).toBeDefined();
+    const allTokens = Array.from(result.keys());
+    expect(allTokens.length).toEqual(1);
+    expect(result.get(CACHE_CONTEXT)).toEqual({ cache: true, ttl: 60000 });
+    expect(result.get(token)).toEqual(0);
+  });
+});
