76fc1e641f1f84652abfdeb9824a9141525110d6
DrDreo <andreas.hahn@bannerflow.com>
2025-06-16 17:41:01 +0200

    chore: migrate to angular@19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,6 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  standalone: true,
   imports: [RouterOutlet, RouterLink],
   styleUrls: ['./app.component.scss']
 })
